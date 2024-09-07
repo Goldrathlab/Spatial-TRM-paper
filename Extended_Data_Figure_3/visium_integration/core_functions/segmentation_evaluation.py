5fc22ad75441881a83b94c2622bbfb928369391b
Maximilian Heeg <maxheeg@gmail.com>
2024-09-07 08:02:15 -0700

    Black formatter

--- Extended_Data_Figure_3/visium_integration/core_functions/segmentation_evaluation.py
+++ Extended_Data_Figure_3/visium_integration/core_functions/segmentation_evaluation.py
@@ -14,25 +14,35 @@ from fast_alphashape import alphashape as alphafast
 import imageio as io
 import json
 
+
 def make_alphashape(points: pd.DataFrame, alpha: float):
     points = np.array(points)
     shape = alphashape.alphashape(points, alpha=alpha)
     return shape
+
 
 def make_alphashape_fast(points: pd.DataFrame, alpha: float):
     points = np.array(points)
     shape = alphafast(points, alpha=alpha)
     return shape
 
+
 def get_pixel_size(path: str) -> float:
     file = open(os.path.join(path, "experiment.xenium"))
     experiment = json.load(file)
-    pixel_size = experiment['pixel_size']
+    pixel_size = experiment["pixel_size"]
     return pixel_size
 
+
 def subset_transcripts_file(transcripts, pixel_size, minx, maxx, miny, maxy):
-    transcript_subset_fov = transcripts[(minx < transcripts.y*(1/pixel_size)) & (transcripts.y*(1/pixel_size) < maxx)& (miny < transcripts.x*(1/pixel_size)) & (transcripts.x*(1/pixel_size) < maxy)]
+    transcript_subset_fov = transcripts[
+        (minx < transcripts.y * (1 / pixel_size))
+        & (transcripts.y * (1 / pixel_size) < maxx)
+        & (miny < transcripts.x * (1 / pixel_size))
+        & (transcripts.x * (1 / pixel_size) < maxy)
+    ]
     return transcript_subset_fov
+
 
 def import_image(path: str):
     file = os.path.join(path, "morphology_mip.ome.tif")
